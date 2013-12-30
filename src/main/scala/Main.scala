abca55e2581f01f7eeebbac531f0d9383f47a8da
Michael Schleichardt <michael@schleichardt.net>
2013-12-30 13:04:06 +0100

    Hello world using simple parameters.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,22 @@ object Main extends App {
   println("running Scala macro experiments")
 }
 
+case class Person(firstName: String, lastName: String)
+
 object MacroImpls {
   import reflect.macros.Context
   def hello_impl(c: Context)(): c.Expr[Unit] = {
     import c.universe._
     reify { println("Hello World!") }
+  }
+
+  //to create the correct parameter lists, use a wrong one and the compiler suggest a good one
+  def helloWithArgs_impl(c: scala.reflect.macros.Context)(message: c.Expr[String], times: c.Expr[Int]): c.Expr[Unit] = {
+    import c.universe._
+    reify {
+      for (i <- 0 until times.splice) {
+        println(message.splice)
+      }
+    }
   }
 }
--- src/test/scala/MacroSpec.scala
+++ src/test/scala/MacroSpec.scala
@@ -15,12 +15,22 @@ class SetSuite extends FunSuite {
     }
     new String(fakeOut.toByteArray) should be("Hello World!\n")
   }
+
+  test("using parameters"){
+    val message = "NaN"
+    val fakeOut = new ByteArrayOutputStream
+    Console.withOut(fakeOut){
+      new Hello().helloWithArgs(message, 3)
+    }
+    new String(fakeOut.toByteArray) should be("NaN\nNaN\nNaN\n")
+  }
 }
 
 object PlayGround {
   import language.experimental.macros
 
   class Hello {
-    def hello(): Unit = macro MacroImpls.hello_impl
+    def hello(): Unit = macro MacroImpls.hello_impl//qualified identifier macro implementation method in a static context
+    def helloWithArgs(message: String, times: Int) = macro MacroImpls.helloWithArgs_impl
   }
 }
